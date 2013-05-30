466be2989365b4241e87d61d1a1e7cd03d0b2c20
edmundo-ferreira <fc.edmundo@gmail.com>
2013-05-30 14:10:20 +0100

    Final working with delete in table. Remove dependency of keyboard and put images and dynamic label coloring

--- termocup_gui.py
+++ termocup_gui.py
@@ -20,10 +20,13 @@ fdic.close()
 
 logging.basicConfig(filename='/home/pi/Desktop/thermocup/debug.log',filemode='w',format="%(levelname)s|%(asctime)-15s|%(message)s",level=logging.WARNING)
 
-title='TermoCup 2013'
+title='ThermoCup 2013'
 title_style='QLabel { font-size: 60pt; color:gray;}'
 timer_style='QLabel {font-size:40pt; color:red;}'
+timer_style_invalid='QLabel {font-size:40pt; color:gray;}'
+timer_style_valid='QLabel {font-size:40pt; color:green;}'
 rfid_style='QLabel {font-size:25pt; color:blue;}'
+rfid_style_invalid='QLabel {font-size:25pt; color:gray;}'
 table_header_style='QHeaderView {font-size: 13pt;  background-color:gray;}'
 table_item_style='QTreeWidget, QTreeView{font-size:12pt; color:black}'
 
@@ -33,23 +36,25 @@ RPIO.setwarnings(False)
 gpio_b_list=[4,17,22,9]
 gpio_e_list=[14,18,23,25]
 
-
 class MainGui(QtGui.QWidget):
 	def __init__(self,parent=None):
 		QtGui.QWidget.__init__(self,parent)
 		screen = QtGui.QDesktopWidget().screenGeometry() #get screen siz		
 		
+		#self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
+
 		#list of reader threads	
 		self.reader_list=list()
 		self.reader_list.append(SerialReader(self,'/dev/ttyACM%s'%0))
 		self.reader_list.append(SerialReader(self,'/dev/ttyACM%s'%1))
+		#self.reader_list.append(SerialReader(self,'/dev/ttyACM%s'%2))
 		self.reader_list.append(SerialReader(self,'/dev/ttyUSB%s'%0))
 
 	
 		#list of track classes
 		self.track_list=list()
 		for i in range(0,len(gpio_b_list)):
-			self.track_list.append(Track(self,gpio_b_list[i],gpio_e_list[i],screen.width()/2-700+i*300))
+			self.track_list.append(Track(self,gpio_b_list[i],gpio_e_list[i],i+1,screen.width()/2-700+i*300))
 
 		#title label
 		self.title= QtGui.QLabel(title, self)
@@ -70,26 +75,29 @@ class MainGui(QtGui.QWidget):
 
 		#ranking table
 		self.tree_widget= QtGui.QTreeWidget(self)
+		self.tree_heder=self.tree_widget.header()
 		self.tree_widget. setHeaderLabels(['Ranking','Team Number','Time (s)','Del'])
-		self.tree_widget.setFixedWidth(800)
-		self.tree_widget.setFixedHeight(500)
-		self.tree_widget.move(screen.width()/2-400,400)
-		self.tree_widget.setColumnWidth(0,200)
+		self.tree_widget.setFixedWidth(1000)
+		self.tree_widget.setFixedHeight(600)
+		self.tree_widget.move(screen.width()/2-500,380)
+		self.tree_widget.setColumnWidth(0,250)
 		self.tree_widget.header().setResizeMode(0,QtGui.QHeaderView.Fixed)
-		self.tree_widget.setColumnWidth(1,200)
+		self.tree_widget.setColumnWidth(1,250)
 		self.tree_widget.header().setResizeMode(1,QtGui.QHeaderView.Fixed)
-		self.tree_widget.setColumnWidth(2,350)
+		self.tree_widget.setColumnWidth(2,450)
 		self.tree_widget.header().setResizeMode(2,QtGui.QHeaderView.Fixed)
 		self.tree_widget.setColumnWidth(3,10)
 		self.tree_widget.header().setResizeMode(3,QtGui.QHeaderView.Fixed)
-		
 		self.tree_widget.header().setDefaultAlignment(QtCore.Qt.AlignCenter) #alingment of header tags
 		self.tree_widget.header().setStyleSheet(table_header_style)
 		self.tree_widget.setStyleSheet(table_item_style)
-					
+	
+		#self.tree_header.setClickable(True)
+		
+		#self.tree_header.itemCliconnect(self.foda)
+		
 		#self.tree_widget.setSortingEnabled(True) 
 		#self.tree_widget.sortItems(2,QtCore.Qt.AscendingOrder)
-
 		self.tree_widget.setAlternatingRowColors(True);
 		
 		
@@ -101,44 +109,66 @@ class MainGui(QtGui.QWidget):
 				new_item.setData(0,QtCore.Qt.DisplayRole,int(aux[0]))
 				new_item.setData(1,QtCore.Qt.DisplayRole,int(aux[1]))
 				new_item.setData(2,QtCore.Qt.DisplayRole,float(aux[2]))
+				new_item.setCheckState(3,QtCore.Qt.Unchecked)
 				new_item.setTextAlignment(0,4)
 				new_item.setTextAlignment(1,4)
 				new_item.setTextAlignment(2,4)
-				new_item.setCheckState(3,QtCore.Qt.Unchecked)
 			else:
 				new_sub_item=QtGui.QTreeWidgetItem(new_item)
 				new_sub_item.setData(1,QtCore.Qt.DisplayRole,int(aux[1]))
 				new_sub_item.setData(2,QtCore.Qt.DisplayRole,float(aux[2]))
+				new_sub_item.setCheckState(3,QtCore.Qt.Unchecked)
 				new_sub_item.setTextAlignment(0,4)
 				new_sub_item.setTextAlignment(1,4)
 				new_sub_item.setTextAlignment(2,4)
-				new_sub_item.setCheckState(3,QtCore.Qt.Unchecked)
 
 		fdata.close()
-		#self.tree_widget.itemDoubleClicked.connect(self.edit_tree_widget)      
-	 	self.tree_widget.itemChanged.connect(self.clikedDel) 
-            #self.setStyleSheet("QWidget { background-color:white }") #set background
+		
+		self.delete_event=QtGui.QShortcut(self)
+		self.delete_event.setKey("Del")
+		self.delete_event.activated.connect(self.deleteItemsSelection)
+
+	      #self.setStyleSheet("QWidget { background-color:white }") #set background
 		self.showFullScreen() #show fullscreen
 
-	def clikedDel(self,item,column):
-		reply = QtGui.QMessageBox.question(self,'Message',"Are you sure you want to delete?", QtGui.QMessageBox.Yes|QtGui.QMessageBox.No,QtGui.QMessageBox.No)
-		if reply == QtGui.QMessageBox.Yes:
-			index=self.tree_widget.indexOfTopLevelItem(item)
-			if index==-1:
-				item.parent().removeChild(item)
-		    	else:
-				self.tree_widget.takeTopLevelItem(index)
-	    	else:
-			item.setCheckState(3,QtCore.Qt.Unchecked)
-			return
 
-	#edit the table
-	#def edit_tree_widget(self,item, index):
-		#print index
-		#if not (index==0 or index==1):
-			#item.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsEditable)           
-			#self.tree_widget.editItem(item, index)       
-		    #  item.setFlags(QtCore.Qt.ItemIsSelectable |  QtCore.Qt.ItemIsEnabled) 
+	def foda(self):
+		print "welcome to foda"
+
+	def deleteItemsSelection(self):
+		reply = QtGui.QMessageBox.question(self,'Message',"Are you sure to quit?", QtGui.QMessageBox.Yes|QtGui.QMessageBox.No,QtGui.QMessageBox.No)
+		if reply == QtGui.QMessageBox.Yes:
+			i=0
+			count_items=self.tree_widget.topLevelItemCount()
+			while i<count_items:
+				aux=self.tree_widget.topLevelItem(i)		
+
+				count_childs=aux.childCount()
+				j=0
+				while j<count_childs:
+					if aux.child(j).checkState(3)==QtCore.Qt.Checked:
+						print "removing child %d of item %d from table"%(j,i)	
+						aux.removeChild(aux.child(j))
+						count_childs=aux.childCount()	
+						j=j-1
+					j=j+1			
+
+
+				if aux.checkState(3)==QtCore.Qt.Checked:
+					if aux.childCount()==0:
+						print "item has no child"
+						print "removing item %d from table"%i
+						self.tree_widget.takeTopLevelItem(i)
+						count_items=self.tree_widget.topLevelItemCount()
+						i=i-1
+					else:
+						print "this item is a parent have mercy on his sole, kill a child instead"
+						aux.setData(2,QtCore.Qt.DisplayRole,float(aux.child(0).text(2)))
+						aux.setCheckState(3,QtCore.Qt.Unchecked)
+						aux.removeChild(aux.child(0))
+
+				i=i+1
+	
 
 	#close safeguard
 	def closeEvent(self, event):
@@ -157,10 +187,11 @@ class MainGui(QtGui.QWidget):
 
 ############################  TRACK CLASS ####################################
 class Track():
-	def __init__(self,gui_data,gpio_b,gpio_e,pos_x):
+	def __init__(self,gui_data,gpio_b,gpio_e,track_id,pos_x):
 		self.gui_data=gui_data	
 		self.gpio_b=gpio_b
 		self.gpio_e=gpio_e
+		self.track_id=track_id
 		#timer control flags 
 		self.timer_running=False
 		self.timer_enable=False
@@ -186,73 +217,97 @@ class Track():
 		self.rfid_label.setAlignment(QtCore.Qt.AlignCenter)
 		self.rfid_label.move(pos_x+60,300)
 	
-	
 	def enableTrack(self,tag_id):
-		print tag_id
 		if self.rfid_enable==True:
+			print "tagid %s enabled track %s"%(tag_id,self.track_id)
+			self.rfid_enable=False
 			self.tag_id=tag_id	
 			self.track_clk.start(10000)
 			self.rfid_label.setText("%s"%tag_id)
-			self.rfid_enable=False
 			self.timer_enable=True
 			RPIO.add_interrupt_callback(self.gpio_b,self.startClk,edge='rising',pull_up_down=RPIO.PUD_UP,debounce_timeout_ms=None)
 
 	def startClk(self,gpio,val):
-		print gpio	
-		if self.timer_running==False and self.timer_enable==True:
+		if self.timer_enable==True:
+			print "tagid %s started run on track %s"%(self.tag_id,self.track_id)
+			self.timer_enable=False
 			RPIO.del_interrupt_callback(self.gpio_b)
 			self.t_0=datetime.datetime.now()
 			self.timer_clk.start(5)
 			self.track_clk.stop()
 			self.timer_running=True
-			self.rfid_enable=False
 			self.gui_data.update()
 			RPIO.add_interrupt_callback(self.gpio_e,self.stopClk,edge='rising',pull_up_down=RPIO.PUD_UP,debounce_timeout_ms=None)
 
 	def stopClk(self,gpio,val):
-		print gpio	
 		if self.timer_running==True: 
+			print "tagid %s ended run on track %s"%(self.tag_id,self.track_id)
+			self.timer_running=False
 			RPIO.del_interrupt_callback(self.gpio_e)
 			self.timer_clk.stop()
-			self.timer_enable=False
-			self.track_clk.start(5000)			
+			self.track_clk.start(5)			
 		
 	#get and set ellapsed time     
 	def setTimerClk(self):
 		self.t_lap= datetime.datetime.now()-self.t_0
 		self.timer_label.setText("%d:%03d"%(self.t_lap.seconds,self.t_lap.microseconds/1000))
 
-	def resetMaster(self):
-		if self.timer_running==True:
-			RPIO.del_interrupt_callback(self.gpio_e)
-			self.timer_clk.stop()
-		self.sortTree(reset=True)
-		self.rfid_label.setText('')	
-		self.timer_label.setText('0:000')	
-		self.rfid_enable=True
-		self.timer_enable=False
-		self.timer_running=False
+			
 
-#disable track and if timer was running kill's interrupt
-	def disableTrack(self):
-		self.track_clk.stop()
-		if self.timer_running==False and self.timer_enable==True:
-			RPIO.del_interrupt_callback(self.gpio_b)
-		else:
-			self.sortTree(reset=False)	
-		self.rfid_label.setText('')	
-		self.timer_label.setText('0:000')	
-		self.timer_running=False
-		self.rfid_enable=True
-################################################################################
+	#disable track and if timer was running kill's interrupt
+	def disableTrack(self,in_number=None):
+		clean=False
+		self.track_clk.stop() #closing it's self
+		if in_number==None:
+			if self.timer_running==False and self.timer_enable==True:
+				print "tagid %s made no run on track %s disabling it "%(self.tag_id,self.track_id)
+				RPIO.del_interrupt_callback(self.gpio_b)
+				clean=True
+			if self.timer_running==False and self.timer_enable==False: 
+				print "tagid %s completed successfull run on track %s disabling it"%(self.tag_id,self.track_id)
+				time.sleep(5)
+				self.sortTree(reset=False)	
+				clean=True
+		if in_number=='MASTER' :
+			if self.timer_running==False and self.timer_enable==False and self.rfid_enable==False:
+				print "%s is canceling after run, tagid %s and disabling track %s"%(in_number,self.tag_id,self.track_id)
+				self.rfid_label.setStyleSheet(rfid_style_invalid)
+				self.timer_label.setStyleSheet(timer_style_invalid)	
+				time.sleep(5)	
+				self.sortTree(reset=True)
+				clean=True
+			if self.timer_running==False and self.timer_enable==True:
+				self.timer_enable=False
+				print "%s is canceling before run, tagid %s and disabling track %s"%(in_number,self.tag_id,self.track_id)
+				clean=True
+			if self.timer_running==True and self.timer_enable==False:
+				self.timer_running=False
+				self.timer_clk.stop()
+				RPIO.del_interrupt_callback(self.gpio_e)
+				self.rfid_label.setStyleSheet(rfid_style_invalid)
+				self.timer_label.setStyleSheet(timer_style_invalid)	
+				print "%s is canceling during run, tagid %s and disabling track %s"%(in_number,self.tag_id,self.track_id)
+				time.sleep(5)	
+				self.sortTree(reset=True)	
+				clean=True
+
+		if clean==True:
+			self.rfid_label.setStyleSheet(rfid_style)
+			self.timer_label.setStyleSheet(timer_style)	
+			self.rfid_label.setText('')	
+			self.timer_label.setText('0:000')	
+			self.rfid_enable=True
+
+
 	def sortTree(self,reset):
 		if reset==False:
 			self.float_timer=float("%d.%03d"%(self.t_lap.seconds,self.t_lap.microseconds/1000))
 		else:
 			self.float_timer=float("inf")
 
+
+
 		match_item=self.gui_data.tree_widget.findItems(self.tag_id,QtCore.Qt.MatchExactly,1)
-		self.gui_data.tree_widget.blockSignals(True)
 		fdata=open('/home/pi/Desktop/thermocup/data/temp.txt','w')
 		if len(match_item)==0:
 			new_item=QtGui.QTreeWidgetItem(self.gui_data.tree_widget)
@@ -279,7 +334,7 @@ class Track():
 			new_sub_item.setTextAlignment(1,4)
 			new_sub_item.setTextAlignment(2,4)
 			new_sub_item.setCheckState(3,QtCore.Qt.Unchecked)
-		
+
 		self.gui_data.tree_widget.sortItems(2,0)
 		print "saving to file"
 		for i in range(0,self.gui_data.tree_widget.topLevelItemCount()):
@@ -290,7 +345,7 @@ class Track():
 				fdata.write(",%d,%.03f\n"%(int(aux.child(j).text(1)),float(aux.child(j).text(2))))
 
 		fdata.close() 
-		self.gui_data.tree_widget.blockSignals(False)
+
 
 
 ############################  SERIAL THREAD  ####################################
@@ -317,7 +372,7 @@ class SerialReader(QtCore.QThread):
 				in_channel=int(in_channel)-1	
 				if not in_number==None and in_channel<len(self.gui_data.track_list):
 					if in_number=='MASTER':
-						  self.gui_data.track_list[in_channel].resetMaster()					
+						  self.gui_data.track_list[in_channel].disableTrack(in_number)					
 					else:
 						  self.gui_data.track_list[in_channel].enableTrack(in_number)
 			except serial.SerialException:
